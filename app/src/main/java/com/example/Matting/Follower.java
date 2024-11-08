f6d582ffbadba3ca8fab5a2bb8d11cd94a61abe4
whtnsrb123 <66052686+whtnsrb123@users.noreply.github.com>
2024-11-08 17:15:01 +0900

    Update Follower.java

--- app/src/main/java/com/example/Matting/Follower.java
+++ app/src/main/java/com/example/Matting/Follower.java
@@ -1,18 +1,14 @@
 package com.example.Matting;
+
+
 public class Follower {
     private String username;
-    private String profileImageUrl;
 
-    public Follower(String username, String profileImageUrl) {
+    public Follower(String username) {
         this.username = username;
-        this.profileImageUrl = profileImageUrl;
     }
 
     public String getUsername() {
         return username;
-    }
-
-    public String getProfileImageUrl() {
-        return profileImageUrl;
     }
 }
