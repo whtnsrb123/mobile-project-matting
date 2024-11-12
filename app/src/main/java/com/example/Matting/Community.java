ce265322a2ae1b56d98fffcee31d5751dc0e6ba3
hsl26 <shuala26@gmail.com>
2024-11-12 17:47:41 +0900

    feat: 커뮤니티 포스트 내용 전달..

--- app/src/main/java/com/example/Matting/Community.java
+++ app/src/main/java/com/example/Matting/Community.java
@@ -1,25 +1,58 @@
 package com.example.Matting;
 
-public class Community {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Community implements Parcelable {
     private String title;
-    private String subtitle;
+    private String content;
     private String info;
 
-    public Community(String title, String subtitle, String info) {
+    public Community(String title, String content, String info) {
         this.title = title;
-        this.subtitle = subtitle;
+        this.content = content;
         this.info = info;
     }
+
+    protected Community(Parcel in) {
+        title = in.readString();
+        content = in.readString();
+        info = in.readString();
+    }
+
+    public static final Creator<Community> CREATOR = new Creator<Community>() {
+        @Override
+        public Community createFromParcel(Parcel in) {
+            return new Community(in);
+        }
+
+        @Override
+        public Community[] newArray(int size) {
+            return new Community[size];
+        }
+    };
 
     public String getTitle() {
         return title;
     }
 
-    public String getSubtitle() {
-        return subtitle;
+    public String getContent() {
+        return content;
     }
 
     public String getInfo() {
         return info;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(content);
+        dest.writeString(info);
     }
 }
