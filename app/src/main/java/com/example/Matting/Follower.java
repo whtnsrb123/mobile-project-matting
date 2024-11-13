f491ecb9cb48a018a6d21362f5974bf6e03f19fd
whtnsrb123 <66052686+whtnsrb123@users.noreply.github.com>
2024-11-13 12:55:00 +0900

    팔로워 추가

--- app/src/main/java/com/example/Matting/Follower.java
+++ app/src/main/java/com/example/Matting/Follower.java
@@ -1,14 +1,25 @@
 package com.example.Matting;
 
-
 public class Follower {
     private String username;
+    private String subText;
+    private int profileImageRes;
 
-    public Follower(String username) {
+    public Follower(String username, String subText, int profileImageRes) {
         this.username = username;
+        this.subText = subText;
+        this.profileImageRes = profileImageRes;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public String getSubText() {
+        return subText;
+    }
+
+    public int getProfileImageRes() {
+        return profileImageRes;
     }
 }
