e83a295fc1b19c593ff07dc91448d7be540a6de4
govl0407 <govl0407@gmail.com>
2024-11-22 10:43:40 +0900

    feat : 채팅창에 유저 닉네임 표시

--- app/src/main/java/com/example/Matting/Chat_Message.java
+++ app/src/main/java/com/example/Matting/Chat_Message.java
@@ -1,9 +1,18 @@
 package com.example.Matting;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Chat_Message {
     private String userId;
     private String message;
     private long timestamp;
+    private String nickName;
 
     public Chat_Message() {
     }
@@ -18,11 +27,31 @@ public class Chat_Message {
         return userId;
     }
 
+    public void getUserNickname(final NicknameCallback callback) {
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("users").child(userId).child("nicknames");
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String nickName = snapshot.getValue(String.class);
+                callback.onCallback(nickName);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // 에러 처리
+            }
+        });
+    }
+
     public String getMessage() {
         return message;
     }
 
     public long getTimestamp() {
         return timestamp;
+    }
+
+    public interface NicknameCallback {
+        void onCallback(String nickName);
     }
 }
--- app/src/main/java/com/example/Matting/User.java
+++ app/src/main/java/com/example/Matting/User.java
@@ -1,36 +1,57 @@
 package com.example.Matting;
 
 import android.content.Context;
+
+import androidx.annotation.NonNull;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class User {
     private String userId;
     private Context context;
     private String email;
+    private String nickName;
 
     // Context를 생성자로 받아서 초기화합니다.
     public User(Context context) {
         this.context = context;
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
-            // Name, email address, and profile photo Url
+            // 이메일 주소 및 ID 설정
             this.email = user.getEmail();
-
-            // Check if user's email is verified
-            boolean emailVerified = user.isEmailVerified();
-
-            // The user's ID, unique to the Firebase project. Do NOT use this value to
-            // authenticate with your backend server, if you have one. Use
-            // FirebaseUser.getIdToken() instead.
             this.userId = user.getUid();
+
+            // 사용자 닉네임 설정
+            DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("users").child(this.userId).child("nicknames");
+            dbRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    nickName = snapshot.getValue(String.class);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    // 에러 처리
+                }
+            });
         }
     }
 
-    public String getEmail() { return this.email; }
+    public String getEmail() {
+        return this.email;
+    }
 
     public String getUserId() {
         return this.userId;
+    }
+
+    public String getNickName() {
+        return this.nickName;
     }
 }
