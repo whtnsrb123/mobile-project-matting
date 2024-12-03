2ecf14b4dfe01b656aea3048d93cb752da3b9f56
hsl26 <shuala26@gmail.com>
2024-12-03 21:59:51 +0900

    feat: 리뷰 사진등록

--- app/src/main/java/com/example/Matting/Main_ReviewAdapter.java
+++ app/src/main/java/com/example/Matting/Main_ReviewAdapter.java
@@ -1,10 +1,17 @@
 package com.example.Matting;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
 import java.util.List;
 
 public class Main_ReviewAdapter extends RecyclerView.Adapter<Main_ReviewAdapter.ReviewViewHolder> {
@@ -27,6 +34,17 @@ public class Main_ReviewAdapter extends RecyclerView.Adapter<Main_ReviewAdapter.
         holder.tvReviewerName.setText(mainReview.getUsername());
         holder.tvReviewContent.setText(mainReview.getContent());
         holder.tvReviewRating.setText("평점: " + mainReview.getRating());
+
+        // 이미지 URL 로드
+        if (mainReview.getImageResource() != null && !mainReview.getImageResource().isEmpty()) {
+            // Base64 문자열을 디코딩
+            byte[] decodedString = Base64.decode(mainReview.getImageResource(), Base64.DEFAULT);
+            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+
+            holder.ivReviewImage.setImageBitmap(decodedByte);
+        } else {
+            holder.ivReviewImage.setImageResource(R.drawable.ic_launcher_background); // 기본 이미지 설정
+        }
     }
 
     @Override
@@ -36,12 +54,14 @@ public class Main_ReviewAdapter extends RecyclerView.Adapter<Main_ReviewAdapter.
 
     public static class ReviewViewHolder extends RecyclerView.ViewHolder {
         TextView tvReviewerName, tvReviewContent, tvReviewRating;
+        ImageView ivReviewImage;
 
         public ReviewViewHolder(@NonNull View itemView) {
             super(itemView);
             tvReviewerName = itemView.findViewById(R.id.tvReviewerName);
             tvReviewContent = itemView.findViewById(R.id.tvReviewContent);
             tvReviewRating = itemView.findViewById(R.id.tvReviewRating);
+            ivReviewImage = itemView.findViewById(R.id.ivReviewImage);
         }
     }
 }
--- app/src/main/java/com/example/Matting/Main_ReviewWriteFragment.java
+++ app/src/main/java/com/example/Matting/Main_ReviewWriteFragment.java
@@ -1,7 +1,14 @@
 package com.example.Matting;
 
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +16,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +36,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main_ReviewWriteFragment extends Fragment {
     private EditText etReviewContent;
     private Button btnSubmit;
@@ -32,10 +48,14 @@ public class Main_ReviewWriteFragment extends Fragment {
     private TextView tvRestaurant;
     private String address, email, username;
     private RatingBar ratingBar;
-
+    private ImageView placeholderImage;
+    private Uri imageUri;
+    private ActivityResultLauncher<Intent> galleryLauncher;
+    private LinearLayout imageUploadButton;
     private User user;
     private FirebaseAuth mAuth;
 
+    @SuppressLint("WrongViewCast")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -65,7 +85,18 @@ public class Main_ReviewWriteFragment extends Fragment {
         tvRestaurant = view.findViewById(R.id.tvRestaurant);
         etReviewContent = view.findViewById(R.id.etReviewContent);
         ratingBar = view.findViewById(R.id.ratingBar);
-        Log.d("ratingBar", String.valueOf(ratingBar));
+        imageUploadButton = view.findViewById(R.id.imageUploadButton);
+        placeholderImage = view.findViewById(R.id.placeholderImage);
+
+        galleryLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == AppCompatActivity.RESULT_OK && result.getData() != null) {
+                        imageUri = result.getData().getData();
+                        placeholderImage.setImageURI(imageUri); // 선택한 이미지 표시
+                    }
+                }
+        );
 
         // 전달받은 데이터 수신
         Bundle arguments = getArguments();
@@ -77,46 +108,89 @@ public class Main_ReviewWriteFragment extends Fragment {
             }
         }
 
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String title = tvRestaurant.getText().toString();
-                String content = etReviewContent.getText().toString();
-                float rating = ratingBar.getRating();
+        // 버튼 클릭 이벤트 설정
+        imageUploadButton.setOnClickListener(v -> openGallery());
 
-                if (!content.isEmpty()) {
-                    // Firebase Firestore에 리뷰 추가
-                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+        btnSubmit.setOnClickListener(v -> {
+            String content = etReviewContent.getText().toString().trim();
+            float rating = ratingBar.getRating();
 
-                    db.collection("review")
-                            .add(new Main_Review(address, content, Timestamp.now(), rating, username)) // username은 예시로 고정값 설정
-                            .addOnSuccessListener(documentReference -> {
-                                Toast.makeText(getActivity(), "리뷰가 저장되었습니다", Toast.LENGTH_SHORT).show();
-
-                                // 프래그먼트 종료 및 이전 프래그먼트로 돌아가기
-                                getParentFragmentManager().popBackStack();
-                            })
-                            .addOnFailureListener(e -> {
-                                Toast.makeText(getActivity(), "리뷰 저장에 실패했습니다", Toast.LENGTH_SHORT).show();
-                            });
+            if (validateInputs(content)) {
+                if (imageUri != null) {
+                    String encodedImage = encodeImageToBase64(imageUri);
+                    if (encodedImage != null) {
+                        saveReviewToFirestore(encodedImage, content, rating); // Firestore에 Base64 이미지 저장
+                    } else {
+                        Toast.makeText(getActivity(), "이미지 인코딩에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                    }
                 } else {
-                    // 내용이 비어있을 경우 메시지 표시
-                    Toast.makeText(getActivity(), "내용을 입력하세요", Toast.LENGTH_SHORT).show();
+                    saveReviewToFirestore(null, content, rating); // 이미지 없이 저장
                 }
             }
         });
 
-
-        btnClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // CreateFragment를 닫고 BackStack에서 이전 프래그먼트(InfoFragment)를 다시 보여줌
-                getParentFragmentManager().popBackStack();
-            }
+        btnClose.setOnClickListener(v -> {
+            // 이전 화면으로 돌아가기
+            getParentFragmentManager().popBackStack();
         });
 
         return view;
     }
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        galleryLauncher.launch(intent); // 새로운 API로 갤러리 시작
+    }
+
+    private boolean validateInputs(String content) {
+        if (content.isEmpty()) {
+            Toast.makeText(getActivity(), "내용을 입력하세요", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void saveReviewToFirestore(String encodedImage, String content, float rating) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> review = new HashMap<>();
+        review.put("address", address);
+        review.put("content", content);
+        review.put("date", Timestamp.now());
+        review.put("rating", rating);
+        review.put("username", username);
+        review.put("imageResource", encodedImage); // Base64로 인코딩된 이미지 저장
+
+        db.collection("review")
+                .add(review)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getActivity(), "리뷰가 저장되었습니다", Toast.LENGTH_SHORT).show();
+                    getParentFragmentManager().popBackStack();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(getActivity(), "리뷰 저장에 실패했습니다", Toast.LENGTH_SHORT).show();
+                    Log.e("Firestore", "리뷰 저장 실패", e);
+                });
+    }
+
+    private String encodeImageToBase64(Uri imageUri) {
+        try {
+            ContentResolver contentResolver = requireContext().getContentResolver();
+            InputStream inputStream = contentResolver.openInputStream(imageUri);
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+            byte[] imageBytes = byteArrayOutputStream.toByteArray();
+
+            return Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 
     private String extractIdFromEmail(String email) {
         if (email.contains("@")) {
