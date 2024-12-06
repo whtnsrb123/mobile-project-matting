93a4d8e1d9a4754b2fa94f9be155e47b30ad5381
whtnsrb123 <66052686+whtnsrb123@users.noreply.github.com>
2024-12-06 12:24:40 +0900

    수정

--- app/src/main/java/com/example/Matting/Follower.java
+++ app/src/main/java/com/example/Matting/Follower.java
@@ -1,14 +1,20 @@
 package com.example.Matting;
 
 public class Follower {
+    private String userId;
     private String username;
     private String subText;
-    private String profileImage; // URL로 변경
+    private String profileImage;
 
-    public Follower(String username, String subText, String profileImage) {
+    public Follower(String userId, String username, String subText, String profileImage) {
+        this.userId = userId;
         this.username = username;
         this.subText = subText;
         this.profileImage = profileImage;
+    }
+
+    public String getUserId() {
+        return userId;
     }
 
     public String getUsername() {
